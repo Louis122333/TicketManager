ffb9e102ea6588d6afa05aad944a2fcf05e7e9fe
llhea <llheadlam@hotmail.com>
2024-07-07 19:49:35 +0200

    add dbcontext

---
+++ TicketManager.Infrastructure/Persistence/TicketManagerDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Domain.Aggregates.Tickets;
+using TicketManager.Domain.Aggregates.Users;
+
+namespace TicketManager.Infrastructure.Persistence
+{
+    public class TicketManagerDbContext : DbContext
+    {
+
+        public TicketManagerDbContext(DbContextOptions<TicketManagerDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TicketManagerDbContext).Assembly);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
