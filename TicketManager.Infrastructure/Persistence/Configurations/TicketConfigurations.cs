bee0ef058fb40b01100c0ea5fb1783724fe40ed6
llhea <llheadlam@hotmail.com>
2024-07-07 19:48:13 +0200

    add entity configurations

---
+++ TicketManager.Infrastructure/Persistence/Configurations/TicketConfigurations.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using TicketManager.Domain.Aggregates.Tickets;
+using TicketManager.Domain.Aggregates.Tickets.Enums;
+using TicketManager.Infrastructure.Persistence.Constants;
+using TicketManager.Domain.Aggregates.Tickets.ValueObjects;
+using TicketManager.Domain.Aggregates.Users.ValueObjects;
+
+namespace TicketManager.Infrastructure.Persistence.Configurations
+{
+    public sealed class TicketConfigurations : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            ConfigureTicketsTable(builder);
+            ConfigureCommentsTable(builder);
+        }
+
+        private static void ConfigureTicketsTable(EntityTypeBuilder<Ticket> builder)
+        {
+            builder
+                .ToTable(TableNames.Tickets);
+
+            builder
+                .HasKey(t => t.Id);
+
+            builder
+                .Property(t => t.Id)
+                .ValueGeneratedNever()
+                .HasConversion(
+                    id => id.Value,
+                    value => TicketId.Create(value));
+            builder
+                .OwnsOne(t => t.Number, nb =>
+                {
+                    nb.Property(n => n.Value)
+                    .HasColumnName("ReferenceNumber")
+                    .IsRequired();
+                });
+
+            builder
+                .Property(t => t.Title)
+                .HasMaxLength(50);
+
+            builder
+                .Property(t => t.Description)
+                .HasMaxLength(200);
+
+            builder
+                .Property(t => t.Type)
+                .HasConversion(
+                v => v.ToString(),
+                v => (TicketType)Enum.Parse(typeof(TicketType), v));
+
+            builder
+                .Property(t => t.Status)
+                .HasConversion(
+                v => v.ToString(),
+                v => (TicketStatus)Enum.Parse(typeof(TicketStatus), v));
+
+            builder
+                .Property(t => t.Priority)
+                .HasConversion(
+                v => v.ToString(),
+                v => (TicketPriority)Enum.Parse(typeof(TicketPriority), v));
+
+            builder
+               .Property(t => t.CreatedBy)
+               .ValueGeneratedNever()
+               .HasConversion(
+                   id => id.Value,
+                   value => UserId.Create(value));
+
+            builder
+            .Property(t => t.AssignedTo)
+            .ValueGeneratedNever()
+            .HasConversion(
+                id => id.Value,
+                value => UserId.Create(value));
+        }
+        private static void ConfigureCommentsTable(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.OwnsMany(c => c.Comments, cb =>
+            {
+                cb.ToTable(TableNames.Comments);
+
+                cb.WithOwner()
+                .HasForeignKey("TicketId");
+
+                cb.HasKey("Id", "TicketId");
+
+                cb.Property(p => p.Id)
+                .HasColumnName("CommentId")
+                .ValueGeneratedNever()
+                .HasConversion(
+                    id => id.Value,
+                    value => CommentId.Create(value));
+
+                cb.Property(c => c.Text)
+                .HasMaxLength(250);
+
+                cb.Property(c => c.CreatedBy)
+                .HasColumnName("CreatedBy")
+                .ValueGeneratedNever()
+                .HasConversion(
+                    id => id.Value,
+                    value => UserId.Create(value));
+
+            });
+        }
+    }
+}
---
+++ TicketManager.Infrastructure/Persistence/Configurations/UserConfigurations.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using TicketManager.Infrastructure.Persistence.Constants;
+using TicketManager.Domain.Aggregates.Users;
+using TicketManager.Domain.Aggregates.Users.ValueObjects;
+using TicketManager.Domain.Aggregates.Users.Enums;
+
+namespace TicketManager.Infrastructure.Persistence.Configurations
+{
+    public sealed class UserConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .ToTable(TableNames.Users);
+
+            builder
+                .HasKey(u => u.Id);
+
+            builder
+                .Property(u => u.Id)
+                .ValueGeneratedNever()
+                .HasConversion(
+                    id => id.Value,
+                    value => UserId.Create(value));
+
+            builder
+                .Property(u => u.FirstName)
+                .HasMaxLength(50);
+
+            builder
+                .Property(u => u.LastName)
+                .HasMaxLength(50);
+
+            builder
+                .Property(u => u.Email)
+                .HasMaxLength(128);
+
+            builder
+                .Property(u => u.Password);
+
+            builder
+                .Property(u => u.Role)
+                .HasConversion(
+                v => v.ToString(),
+                v => (UserRole)Enum.Parse(typeof(UserRole), v));
+        }
+    }
+}
