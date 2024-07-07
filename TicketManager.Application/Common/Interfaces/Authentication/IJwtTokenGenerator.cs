a0ed8667f1285ed4f97c3f4cefad7a8bb2ab43ba
llhea <llheadlam@hotmail.com>
2024-07-07 19:46:41 +0200

    add application interfaces

---
+++ TicketManager.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,17 @@
+﻿using TicketManager.Domain.Aggregates.Users;
+
+namespace TicketManager.Application.Common.Interfaces.Authentication
+{
+    /// <summary>
+    /// Interface for generating JWT tokens for users.
+    /// </summary>
+    public interface IJwtTokenGenerator
+    {
+        /// <summary>
+        /// Generates a JWT token for the specified user.
+        /// </summary>
+        /// <param name="user">The user for whom the token is generated.</param>
+        /// <returns>The generated JWT token as a string.</returns>
+        string GenerateToken(User user);
+    }
+}
---
+++ TicketManager.Application/Common/Interfaces/Authentication/IPasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿namespace TicketManager.Application.Common.Interfaces.Authentication
+{
+    /// <summary>
+    /// Interface for hashing and verifying passwords.
+    /// </summary>
+    public interface IPasswordHasher
+    {
+        /// <summary>
+        /// Hashes the provided password using a cryptographic hash method.
+        /// </summary>
+        /// <param name="password">The password to hash.</param>
+        /// <returns>The hashed password as a string.</returns>
+        string Hash(string password);
+
+        /// <summary>
+        /// Verifies whether the input password matches the provided hashed password.
+        /// </summary>
+        /// <param name="passwordHash">The previously hashed password.</param>
+        /// <param name="inputPassword">The password to verify.</param>
+        /// <returns>True if the input password matches the hashed password; otherwise, false.</returns>
+        bool Verify(string passwordHash, string inputPassword);
+    }
+}
