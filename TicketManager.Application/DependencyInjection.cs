0f98f87c2fc9dc16487ab3c9130410f7ed84f306
llhea <llheadlam@hotmail.com>
2024-07-07 19:47:15 +0200

    add application di servicecollection

---
+++ TicketManager.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using TicketManager.Application.Common.Behaviors;
+
+namespace TicketManager.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+
+            services.AddScoped(
+            typeof(IPipelineBehavior<,>),
+            typeof(LoggingPipelineBehavior<,>));
+
+            ValidatorOptions.Global.LanguageManager.Enabled = false;
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
