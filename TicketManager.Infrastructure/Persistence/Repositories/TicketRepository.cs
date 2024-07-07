8d439b78d845eb59b1cfcadadb1efcad12eb85c8
llhea <llheadlam@hotmail.com>
2024-07-07 19:49:07 +0200

    implement entity repositories

---
+++ TicketManager.Infrastructure/Persistence/Repositories/TicketRepository.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Domain.Aggregates.Tickets.Entities;
+using TicketManager.Domain.Aggregates.Tickets.Enums;
+using TicketManager.Domain.Aggregates.Tickets.Interfaces;
+using TicketManager.Domain.Aggregates.Tickets.ValueObjects;
+using TicketManager.Domain.Aggregates.Tickets;
+using TicketManager.Domain.Aggregates.Users.ValueObjects;
+
+namespace TicketManager.Infrastructure.Persistence.Repositories
+{
+    public class TicketRepository : ITicketRepository
+    {
+        private readonly TicketManagerDbContext _context;
+
+        public TicketRepository(TicketManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Ticket ticket)
+        {
+            _context.Tickets.Add(ticket);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddCommentAsync(Ticket ticket, Comment comment)
+        {
+            ticket.AddComment(comment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<Ticket>> GetAllAsync()
+        {
+            return await _context.Tickets.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<Ticket>> GetByUserIdAsync(UserId userId)
+        {
+            return await _context.Tickets
+                 .AsNoTracking()
+                 .Where(t => t.CreatedBy == userId)
+                 .ToListAsync();
+        }
+
+        public async Task<Ticket?> GetByIdAsync(TicketId id)
+        {
+            return await _context.Tickets
+                .Include(t => t.Comments)
+                .SingleOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<IReadOnlyList<Ticket>> GetByTypeAsync(TicketType type)
+        {
+            return await _context.Tickets
+                .AsNoTracking()
+                .Where(t => t.Type == type)
+                .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<Ticket>> GetByStatusAsync(TicketStatus status)
+        {
+            return await _context.Tickets
+                .AsNoTracking()
+                .Where(t => t.Status == status)
+                .ToListAsync();
+        }
+
+        public async Task<Ticket> UpdatePriorityAsync(Ticket ticket, TicketPriority ticketPriority)
+        {
+            ticket.UpdatePriority(ticketPriority);
+            await _context.SaveChangesAsync();
+
+            return ticket;
+        }
+
+        public async Task<Ticket> UpdateStatusAsync(Ticket ticket, TicketStatus ticketStatus)
+        {
+            ticket.UpdateStatus(ticketStatus);
+            await _context.SaveChangesAsync();
+
+            return ticket;
+        }
+
+        public async Task<Ticket> AssignToUserAsync(Ticket ticket, UserId assignedTo)
+        {
+            _context.Tickets.Attach(ticket);
+            ticket.AssignToUser(assignedTo);
+            _context.Entry(ticket).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return ticket;
+        }
+
+        public async Task<bool> ExistsByReferenceNumberAsync(ReferenceNumber referenceNumber)
+        {
+            return await _context.Tickets
+                .AnyAsync(t => t.Number.Value == referenceNumber.Value);
+        }
+    }
+}
---
+++ TicketManager.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Domain.Aggregates.Users;
+using TicketManager.Domain.Aggregates.Users.Interfaces;
+using TicketManager.Domain.Aggregates.Users.ValueObjects;
+
+namespace TicketManager.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TicketManagerDbContext _context;
+
+        public UserRepository(TicketManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<User>> GetAllAsync()
+        {
+            return await _context.Users.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Email == email) ?? null;
+        }
+
+        public async Task<User?> GetByIdAsync(UserId userId)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
