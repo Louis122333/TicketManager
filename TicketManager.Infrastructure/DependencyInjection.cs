468962ae72d9beec6b4c2010e974502a9b9a103d
llhea <llheadlam@hotmail.com>
2024-07-07 19:49:48 +0200

    add di container

---
+++ TicketManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Text;
+using TicketManager.Application.Common.Interfaces.Authentication;
+using TicketManager.Application.Common.Interfaces.Services;
+using TicketManager.Domain.Aggregates.Tickets.Interfaces;
+using TicketManager.Domain.Aggregates.Users.Interfaces;
+using TicketManager.Infrastructure.Authentication;
+using TicketManager.Infrastructure.Persistence.Data;
+using TicketManager.Infrastructure.Persistence.Repositories;
+using TicketManager.Infrastructure.Persistence;
+using TicketManager.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TicketManager.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            ConfigurationManager configuration)
+        {
+            services.AddAuth(configuration)
+                    .AddPersistence(configuration);
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+            services.AddScoped<DataSeeder>();
+            return services;
+        }
+
+        private static IServiceCollection AddPersistence(
+          this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddDbContext<TicketManagerDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddAuth(
+              this IServiceCollection services,
+              ConfigurationManager configuration)
+        {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+            services.AddSingleton(Options.Create(jwtSettings));
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                });
+
+            return services;
+        }
+    }
+}
