9be9eec2c3447a4763ff5ef4825e749a1356d02f
llhea <llheadlam@hotmail.com>
2024-07-07 19:51:05 +0200

    add di for mapping

---
+++ TicketManager.Api/DependencyInjection.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.OpenApi.Models;
+using TicketManager.Api.Common.Errors;
+using TicketManager.Api.Common.Mapping;
+
+namespace TicketManager.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddMappings();
+            services.AddControllers();
+            services.AddSingleton<ProblemDetailsFactory, TicketManagerProblemDetailsFactory>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+            return services;
+        }
+    }
+}
