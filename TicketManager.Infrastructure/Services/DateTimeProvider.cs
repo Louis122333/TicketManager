eb1f958465a70685e94c96a4e741f328dab2f9f4
llhea <llheadlam@hotmail.com>
2024-07-07 19:49:28 +0200

    add datetime service

---
+++ TicketManager.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using TicketManager.Application.Common.Interfaces.Services;
+
+namespace TicketManager.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
