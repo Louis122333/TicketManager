6a4de4b3e4da209a02234f83f6d13e132734a46d
llhea <llheadlam@hotmail.com>
2024-07-11 00:20:26 +0200

    add ticket and comment id

--- TicketManager.Api/Common/Mapping/TicketMappingConfig.cs
+++ TicketManager.Api/Common/Mapping/TicketMappingConfig.cs
@@ -10,20 +10,22 @@ namespace TicketManager.Api.Common.Mapping
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<Ticket, TicketResponse>()
-            .Map(dest => dest.ReferenceNumber, src => src.Number.Value)
-            .Map(dest => dest.Title, src => src.Title)
-            .Map(dest => dest.Status, src => src.Status)
-            .Map(dest => dest.AssignedTo, src => src.AssignedTo.Value);
+                .Map(dest => dest.TicketId, src => src.Id.Value.ToString())
+                .Map(dest => dest.ReferenceNumber, src => src.Number.Value)
+                .Map(dest => dest.Title, src => src.Title)
+                .Map(dest => dest.Status, src => src.Status)
+                .Map(dest => dest.AssignedTo, src => src.AssignedTo.Value);
 
             config.NewConfig<Ticket, TicketDetailedResponse>()
-           .Map(dest => dest.TicketId, src => src.Id.Value.ToString())
-           .Map(dest => dest.ReferenceNumber, src => src.Number.Value)
-           .Map(dest => dest.CreatedDateTime, src => src.CreatedDateTime.ToLocalTime())
-           .Map(dest => dest.UpdatedDateTime, src => src.UpdatedDateTime.ToLocalTime())
-           .Map(dest => dest.CreatedBy, src => src.CreatedBy.Value)
-             .Map(dest => dest.AssignedTo, src => src.AssignedTo.Value);
+                .Map(dest => dest.TicketId, src => src.Id.Value.ToString())
+                .Map(dest => dest.ReferenceNumber, src => src.Number.Value)
+                .Map(dest => dest.CreatedDateTime, src => src.CreatedDateTime.ToLocalTime())
+                .Map(dest => dest.UpdatedDateTime, src => src.UpdatedDateTime.ToLocalTime())
+                .Map(dest => dest.CreatedBy, src => src.CreatedBy.Value)
+                 .Map(dest => dest.AssignedTo, src => src.AssignedTo.Value);
 
             config.NewConfig<Comment, CommentResponse>()
+                .Map(dest => dest.CommentId, src => src.Id.Value.ToString())
                 .Map(dest => dest.CreatedBy, src => src.CreatedBy.Value)
                 .Map(dest => dest.CreatedDateTime, src => src.CreatedDateTime.ToLocalTime());
         }
